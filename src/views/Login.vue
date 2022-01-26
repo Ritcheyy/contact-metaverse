5b08e222075befd5abec1fd97e1d2a7731748051
Ritcheyy <oluseyer@gmail.com>
2022-01-26 08:47:11 +0100

    project setup, login route

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+      <div>Kippa logo</div>
+      <login-form></login-form>
+      <p>c Kippa 2021</p>
+
+          font-family: "DM Sans",serif!important;
+
+          https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&amp;display=swap
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
